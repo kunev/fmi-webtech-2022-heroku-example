08a141862bcd7b0d92500ec8685a9f335f99841c
Evgeni Kunev <evgeni@kunev.me>
2022-05-20 16:58:01 +0300

    Use PORT env variable in production

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const pokemonApiRouter = require("./api");
 
 const app = express();
 
+const serverProps = {};
+
 if (process.env.NODE_ENV === 'development') {
   const cors = require('cors');
   app.use(cors());
+  serverPrompt.port = 3001;
 } else if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static('static'));
+  serverProps.port = process.env.PORT;
 }
 
 app.use(express.json());
 app.use('/api', pokemonApiRouter);
 
 
-app.listen(3001, () => {console.log('server running')});
+app.listen(serverProps.port, () => {console.log(`server running on port ${serverProps.port}`)});
